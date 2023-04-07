7802bc74d203532e2946c0638da9657398144098
Jacob Spackman <jacobts202@gmail.com>
2023-04-06 19:57:30 -0600

    complete

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const button = document.querySelector('button')
 const pressBtn = () => {
     axios.get("https://swapi.dev/api/planets/?search=ald")
     .then(response => {
-        for (let i=0; i<response.data.results.residents.length; i++){
-            axios.get(response[i]).then(response => {
+        for (let i=0; i<response.data.results[0].residents.length; i++){
+            axios.get(response.data.results[0].residents[i]).then(response => {
                 const newName = document.createElement('h2')
                 newName.textContent = `${response.data.name}`
                 document.body.appendChild(newName)
